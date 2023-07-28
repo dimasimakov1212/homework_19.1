52bd8899f96222e4424bda16b51deb146b599ddc
DimaSimakov <dimarodeo@gmail.com>
2023-07-28 20:00:46 +0700

    add comments in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
+# параметры подключения
 hostName = "localhost"
 serverPort = 8080
 
@@ -8,6 +9,10 @@ serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
 
     def __get_html_content(self):
+        """
+        Считывает содержимое html файла
+        :return:
+        """
         html_file = 'homework.html'
         with open(html_file, "r", encoding="utf-8") as file:
             html_content = file.read()
@@ -15,23 +20,30 @@ class MyServer(BaseHTTPRequestHandler):
         return html_content
 
     def do_GET(self):
+        """
+        Обрабатывает запросы на сервер
+        :return:
+        """
 
         shop_html = self.__get_html_content()
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/html")  # Тип данных, передаваемых в запросе
         self.end_headers()
-        self.wfile.write(bytes(shop_html, "utf-8"))
+        self.wfile.write(bytes(shop_html, "utf-8"))  # Вывод информации
 
 
 if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку в классе MyServer
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        webServer.serve_forever()
+        webServer.serve_forever()  # Cтарт веб-сервера в бесконечном цикле запросов
     except KeyboardInterrupt:
         pass
 
+    # Остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
